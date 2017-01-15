9be46e6d7fe88b1a2d5f456d56b02bebe7ec0376
yashodas <yashwalter@gmail.com>
2017-01-15 13:41:32 -0600

    ReviewMonkey

--- reviewMonkey/scripts/app.js
+++ reviewMonkey/scripts/app.js
@@ -1,7 +1,7 @@
 //Amazon API url
 var AmazonBaseUrl = 'https://tjstalcup.site/apac-php/index.php';
 //youtube API url
-//var YoutubeBaseUrl = 'https://www.googleapis.com/youtube/v3/search';
+var YoutubeBaseUrl = 'https://www.googleapis.com/youtube/v3/search';
 
 //Function to get the data from the api
 function getDataFromApi(searchTerm, callback) {
@@ -23,9 +23,9 @@ function displayAmazonSearchData(data) {
   var resultElement = '';
   if (data.Items.Item) {
     data.Items.Item.forEach(function(item) {
-     resultElement += '<p>' + item.ItemAttributes.Title + ' - '
+     resultElement += '<div class="result-item"><p><span class="result-item-title">' + item.ItemAttributes.Title + '</span> - '
                       +item.ItemAttributes.ListPrice.FormattedPrice+'</p>';
-     resultElement += '<p><img src="' + item.MediumImage.URL + '"/></p>';
+     resultElement += '<p><img src="' + item.MediumImage.URL + '"/></p></div>';
     });
   }
   else {
@@ -42,7 +42,20 @@ function watchSubmit() {
     getDataFromApi(query, displayAmazonSearchData);
   });
 }
+//function to display youtube results when selected on the product
+function displayYoutubeSearchResults(){
+  
+
+}
+//jquery function to click on the amazon product
+function clickAmazonItem(){
+  $('.js-search-results').on('click','.result-item',function(){
+    var query = $(this).find('span.result-item-title').text();
+    alert(query);
+  });
+}
 
 $(function(){
   watchSubmit();
+  clickAmazonItem();
 });
